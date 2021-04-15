2b389d8aa7fe06124fd4c962600c20ae29c79d10
Illia <78841712+crawuk@users.noreply.github.com>
2021-04-15 13:59:57 +0300

    Add files via upload

---
+++ Start_game.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef Start_game_h
+#define Start_game_h
+
+
+
+class Begin
+{
+private:
+    
+    std::string intro_str = "Hello and welcome to mini-game!\nYour goal is to reach the 5th level of authority\nAuthority level can be gained by killing bosses that are already waiting for you!\n";
+    
+public:
+    
+    void get_intro_msg();
+    
+};
+
+
+#endif /* Start_game_h */
--- main.cpp
+++ main.cpp
@@ -1,71 +1,18 @@
 #include <iostream>
-#include "sum_func.h"
-#include "human_class.h"
-#include "myclass.h"
-#include "point.h"
+#include <string>
+#include <cmath>
+#include "Start.h"
+#include "Start_game.h"
+
 using namespace std;
 
-// classes
-
-class testclass
+int main(int argc, const char * argv[])
 {
-private:
-    int arr[5] = {1,3,5,7,9};
-    int var = 8;
-    friend void changeX (testclass &value); // <- дружественная функция, которая может получить доступ к private полям класса. функция может быть дружественна к нескольким классам void changeX (point &value, myclass &smth), например
-public:
-    
-    int & operator [] (int index )  // перегрузка оператора индексирования []
-    {
-        return arr[index];
-    }
-    
-};
-
-void changeX (testclass &value)
-{
-    value.arr[0] = 98;
-}
-
-int main()
-{
-    
-    Human alex;
-    alex.age = 25;
-    alex.surname = "alexandrov";
-    alex.weight = 95;
-    alex.print();
-    
-    Human natasha;
-    natasha.weight = 45;
-    natasha.age = 20;
-    natasha.surname = "ivanova";
-    natasha.print();
-    
-    
-    
-    point a(5, 44);
-    a.print();
-    a.SetX(78);
-    a.print();
-    cout << a.GetX() << endl;
-    
-    point p;
-    p.print();
-    
-    point n;
-    n.SetXWithThisKeyword(6);
-    n.print();
-    
-    
-    myclass d(1);
-    myclass e(d);
-    
-    /*
-    testclass yt;
-    cout << yt[3] << endl;
-    */
-    
+    Character player;
+    player.get_intro_msg();
+    player.get_name_from_user();
+    player.get_name();
+    player.choose_a_class();
     
     return 0;
 }
